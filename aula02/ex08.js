197cffbfe70b3bc582c0ab9d98c87df8f3283ab1
Allanzinho15 <allanhot2016@gmail.com>
2025-06-08 19:52:50 -0300

    atualizaçao de atividade

--- aula02/ex08.js
+++ aula02/ex08.js
@@ -1,8 +1,9 @@
 let vel = 80
 console.log(`a velocidade do seu carro é ${vel}km`)
-if ( vel = 81 ){
+if ( vel >= 81 ){
     console.log(" vocé foi multado")
-} 
-    console.log("seu carro esta na velocidade adequada")
+} else{
+    console.log(`seu carro esta na velocidade adequada`)
+}
 
 
